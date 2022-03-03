ddef522050fa87fbf896b08ea9f76ae10bc28dcb
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2022-03-03 17:55:13 +0300

    initial sequelize migration configs

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();module.exports = {
+  development: {
+    username: process.env.DB_USER_DEV,
+    password: process.env.DB_PASS_DEV,
+    database: process.env.DB_DEV,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.DB_USER_TEST,
+    password: process.env.DB_PASS_TEST,
+    database: process.env.DB_TEST,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USER_TEST,
+    password: process.env.DB_PASS_TEST,
+    database: process.env.DB_TEST,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+  },
+};
