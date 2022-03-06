744efd37a168e7779a52ee7b702c8910e52fbd35
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2022-03-06 17:06:33 +0300

    add routes for tasks

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const UsersController = require('../controllers/usersController');
+const TasksController = require('../controllers/tasksController');
 
 router.post('/personel/signup', UsersController.signup);
 router.post('/personel/login', UsersController.login);
+router.post('/tasks', TasksController.createTask);
+router.get('/tasks', TasksController.listAndCountTasks);
 
 module.exports = router;
