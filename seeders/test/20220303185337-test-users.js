4a525f35bc171cb2be1ef761ba2db74db629b002
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2022-03-06 17:02:59 +0300

    create seeders for test environment

--- seeders/test/20220303185337-test-users.js
+++ seeders/test/20220303185337-test-users.js
@@ -1,47 +1,55 @@
 'use strict';
+const bcrypt = require('bcrypt');
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-          await queryInterface.bulkInsert('Users', [{
-           phone: '0721123451',
-            password: '123456',
-            createdAt: new Date(),
-           updatedAt: new Date()
-          },
-            {
-              phone: '0721123452',
-              password: '123456',
-              createdAt: new Date(),
-              updatedAt: new Date()
-            },
-            {
-              phone: '0721123453',
-              password: '123456',
-              createdAt: new Date(),
-              updatedAt: new Date()
-            },
-            {
-              phone: '0721123454',
-              password: '123456',
-              createdAt: new Date(),
-              updatedAt: new Date()
-            },
-            {
-              phone: '0721123455',
-              password: '123456',
-              createdAt: new Date(),
-              updatedAt: new Date()
-            },
-            {
-              phone: '0721123456',
-              password: '123456',
-              createdAt: new Date(),
-              updatedAt: new Date()
-            }
-          ], {})
+  async up(queryInterface, Sequelize) {
+    // This is not a good practice for hashing, since the hashed password will not be unique
+    const rawPassword = '123456';
+    let hashedPassword;
+    const salt = bcrypt.genSaltSync();
+    hashedPassword = bcrypt.hashSync(rawPassword, salt);
+    await queryInterface.bulkInsert('Users', [{
+      phone: '0721123451',
+      password: hashedPassword,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      phone: '0721123452',
+      password: hashedPassword,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      phone: '0721123453',
+      password: hashedPassword,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      phone: '0721123454',
+      password: hashedPassword,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      phone: '0721123455',
+      password: hashedPassword,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      phone: '0721123456',
+      password: hashedPassword,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+    ], {
+      individualHooks: true,
+    })
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete('Users', null, {});
   }
 };
--- seeders/test/20220303185347-test-tasks.js
+++ seeders/test/20220303185347-test-tasks.js
@@ -1,24 +1,36 @@
 'use strict';
-
+const _ = require('lodash');
+const { faker } = require('@faker-js/faker');
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    const tasksBulk = _.times(1000, () => ({ 
+      customer_first_name: faker.name.firstName(),
+      personnel_first_name: faker.name.firstName(),
+      personnel_other_name: faker.name.findName(),
+      customer_last_name: faker.name.lastName(),
+      customer_phone: faker.phone.phoneNumber('+2547########'),
+      agentId: faker.random.alphaNumeric(4),
+      assigned: new Date(),
+      in_progress: new Date(),
+      completed: faker.random.alpha(4),
+      deferred: new Date(),
+      status: faker.random.alpha(4),
+      location: faker.random.alpha(4),
+      gender: faker.random.alpha(4),
+      age: faker.datatype.number(100),
+      access_code: faker.datatype.number(),
+      splash_page: faker.random.alphaNumeric(4),
+      mpesa: faker.random.alphaNumeric(4),
+      autoplay: faker.random.alphaNumeric(4),
+      comments: faker.random.alphaNumeric(10),
+      registration: faker.random.alpha(4),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }));
+    await queryInterface.bulkInsert('Tasks', tasksBulk, {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+     await queryInterface.bulkDelete('Tasks', null, {});
   }
 };
