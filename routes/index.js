ad392387563fc52829504e2189e5b101e0e2fa06
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2022-03-07 23:10:52 +0300

    secure the tasks routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const UsersController = require('../controllers/usersController');
 const TasksController = require('../controllers/tasksController');
+const Auth = require('../middleware/auth');
 
 router.post('/personel/signup', UsersController.signup);
 router.post('/personel/login', UsersController.login);
-router.post('/tasks', TasksController.createTask);
-router.get('/tasks', TasksController.listAndCountTasks);
+router.post('/tasks', Auth.verifyToken,TasksController.createTask);
+router.get('/tasks', Auth.verifyToken,TasksController.listAndCountTasks);
 
 module.exports = router;
